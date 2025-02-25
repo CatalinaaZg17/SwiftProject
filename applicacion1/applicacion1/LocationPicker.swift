ecf6f9e19154115b55a6dc159678da63a2c66a56
rodriamarog <rodriamarog@gmail.com>
2025-02-24 18:56:33 -0800

    Cambios a location picker

--- applicacion1/applicacion1/LocationPicker.swift
+++ applicacion1/applicacion1/LocationPicker.swift
@@ -2,6 +2,29 @@
 //  LocationPicker.swift
 //  applicacion1
 //
-//  Created by Oz bibriesca on 2/24/25.
+//  Created by Rodrigo on 2/24/25.
 //
+import SwiftUI
+import MapKit
+import PhotosUI
+
+// Vista del selector de ubicación
+struct LocationPicker: View {
+    @Binding var location: CLLocationCoordinate2D
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 0, longitude: 0),
+        span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true)
+            .onTapGesture { gesture in
+                let mapCenter = region.center
+                location = mapCenter
+            }
+
+    }
+}
+Escribe en Catalina ZFranco y Oscar A Mercado Bibriesca
+
 
