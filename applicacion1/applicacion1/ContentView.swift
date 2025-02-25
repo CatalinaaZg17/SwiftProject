342e7a0cdc9a2fea9ec0aeda44caafd9e7562d83
CatalinaZ <zavala.catalina17@gmail.com>
2025-02-24 18:56:39 -0800

    Content view

--- applicacion1/applicacion1/ContentView.swift
+++ applicacion1/applicacion1/ContentView.swift
@@ -6,19 +6,52 @@
 //
 
 import SwiftUI
+import MapKit
+import PhotosUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello mama, world!")
-        }
-        .padding()
+// Modelo de datos
+struct LocationItem: Identifiable {
+    let id = UUID()
+    let country: String
+    let coordinate:CLLocationCoordinate2D
+    let image: UIImage
+}
+
+// ViewModel
+class LocationViewModel: ObservableObject {
+    @Published var locations: [LocationItem] = []
+    
+    func addLocation(country: String, coordinate: CLLocationCoordinate2D, image: UIImage) {
+        let newItem = LocationItem(country: country, coordinate: coordinate, image: image)
+        locations.append(newItem)
+    }
+    func removeLocation(id: UUID) {
+        locations.removeAll { $0.id == id }
     }
 }
 
-#Preview {
-    ContentView()
-}
+struct ContentView: View {
+    @StateObject private var viewModel = LocationViewModel()
+    @State private var showAddLocation = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                MapView(locations: viewModel.locations)
+                    .frame(height: 300)
+                
+                LocationListView(viewModel: viewModel)
+ 
+            }
+            .navigationTitle("Lugares Guardados")
+            .toolbar {
+                Button("+") {
+                    showAddLocation.toggle()
+                }
+            }
+            .sheet(isPresented: $showAddLocation) {
+            AddLocationView(viewModel: viewModel, showAddLocation: $showAddLocation)
+            }
+            }
+        }
+    }
