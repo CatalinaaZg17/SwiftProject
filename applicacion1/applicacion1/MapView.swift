a860b7973b9e1bf4c916366974132488a0bc3555
Oscar Mercado <Oscar.mercado@cetys.edu.mx>
2025-02-24 19:16:09 -0800

    Mapview

--- applicacion1/applicacion1/MapView.swift
+++ applicacion1/applicacion1/MapView.swift
@@ -5,3 +5,20 @@
 //  Created by Oz bibriesca on 2/24/25.
 //
 
+import SwiftUI
+import MapKit
+import PhotosUI
+
+struct MapView: View {
+    var locations: [LocationItem]
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 20.0, longitude: 0.0),
+        span: MKCoordinateSpan(latitudeDelta: 50, longitudeDelta: 50)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region, annotationItems: locations) { location in
+            MapMarker(coordinate: location.coordinate, tint: .blue)
+        }
+    }
+}
