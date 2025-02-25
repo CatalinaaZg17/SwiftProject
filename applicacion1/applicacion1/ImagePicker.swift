507bfdf27c8a4707d8a8fa2ea5eefd69c8628eb9
rodriamarog <rodriamarog@gmail.com>
2025-02-24 18:55:06 -0800

    Se agrego el image picker

--- applicacion1/applicacion1/ImagePicker.swift
+++ applicacion1/applicacion1/ImagePicker.swift
@@ -2,6 +2,40 @@
 //  ImagePicker.swift
 //  applicacion1
 //
-//  Created by Oz bibriesca on 2/24/25.
+//  Created by Rodrigo on 2/24/25.
 //
+import SwiftUI
+import MapKit
+import PhotosUI
 
+// Vista del selector de imágenes
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            picker.dismiss(animated: true)
+        }
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+}
